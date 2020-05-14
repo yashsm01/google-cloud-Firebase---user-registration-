6f1d9d17cbda9d9bdfbc969f3d44597f4cbc0859
yashsm01 <yashsm01@gmail.com>
2020-05-14 15:47:27 +0530

    Firebase user registration

---
+++ app/src/main/java/com/example/yashhomeautomation/ui/firebase.java
@@ -0,0 +1,19 @@
+package com.example.yashhomeautomation.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+import com.example.yashhomeautomation.R;
+
+public class firebase extends AppCompatActivity {
+    private Button mSendData;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_firebase);
+    }
+}
